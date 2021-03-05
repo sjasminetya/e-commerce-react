c0cd9fe950c162b246283ff86c47f3dd8a7e4fdb
sjasminetya <shaulajasminetya22@gmail.com>
2021-03-05 21:16:32 +0700

    add header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react'
+// import {BrowserRouter, Route} from 'react-router-dom'
+import HeaderComponent from './components/Header/HeaderComponent'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderComponent />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Header/HeaderComponent.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarText,
+    InputGroup, 
+    InputGroupText, 
+    Input
+} from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faStoreAlt, faEnvelope, faBell, faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
+
+function HeaderComponent() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="white" light expand="md" className="shadow-sm p-3 mb-5 bg-body rounded">
+                    <div className="ml-3 mr-3 pt-1 brand">
+                        <h5 className="text-dark pt-2 pb-3 pl-3">ECOMMERCE</h5>
+                    </div>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <NavbarText>Kategori</NavbarText>
+                        <InputGroup style={{width: "55%"}} className="ml-3">
+                            <Input placeholder="search product" />
+                            <InputGroupText><FontAwesomeIcon icon={faSearch} /></InputGroupText>
+                        </InputGroup>
+                        <div className="ml-auto">
+                            <FontAwesomeIcon className="mr-4" icon={faShoppingCart} />
+                            <FontAwesomeIcon className="mr-4" icon={faBell} />
+                            <FontAwesomeIcon className="mr-5" icon={faEnvelope} />
+                            <NavbarText className="mr-5"><FontAwesomeIcon icon={faStoreAlt} /> Toko</NavbarText>
+                            <NavbarText className="mr-3"><FontAwesomeIcon icon={faUser} /> Muhammad</NavbarText>
+                        </div>
+                    </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default HeaderComponent
