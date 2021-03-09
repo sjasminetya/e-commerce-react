11afd0028eb0336321e8aee8fdc22a7fd1af3672
sjasminetya <shaulajasminetya22@gmail.com>
2021-03-09 07:48:44 +0700

    responsive modal cart

--- src/components/Chart/CartComponent.js
+++ src/components/Chart/CartComponent.js
@@ -16,14 +16,16 @@ function CartComponent(props) {
         <div>
             <Modal isOpen={props.modal} toggle={props.toggle} className="modal-lg">
                 <ModalHeader toggle={props.toggle} style={{color: "rgba(0,0,0,.5)"}}>Berhasil Ditambahkan</ModalHeader>
-                <ModalBody>
+                <ModalBody style={{minHeight: "200px"}}>
                     {props.chart.map(item => (
                         item.id === Number(id) ? (
-                            <div className="d-flex" key={item.id}>
-                                <div className="image-chart">
-                                    <img src={item.imgUrl} alt=""/>
+                            <div className="d-flex cart-modal-detail" key={item.id}>
+                                <div className="cart-modal">
+                                    <div className="image-chart">
+                                        <img src={item.imgUrl} alt=""/>
+                                    </div>
+                                    <h5 className="ml-2 title" style={{color: "rgba(0,0,0,.5)", border: "1px solid red"}}>{item.title}</h5>
                                 </div>
-                                <h5 className="ml-3 mt-4" style={{color: "rgba(0,0,0,.5)"}}>{item.title}</h5>
                                 <Button className="btn-success float-right mr-3 mt-4" style={{height: "fit-content", borderRadius: "7px"}} onClick={goChart}>Lihat Keranjang</Button>
                             </div>
                         ) : null
